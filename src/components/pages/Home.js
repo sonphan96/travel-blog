bae32835251d2886db65df375f02e54ba9a1e60b
johnson <phantruongson96@gmail.com>
2021-03-16 15:32:59 -0400

    final

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -2,12 +2,14 @@ import React from 'react';
 import '../../App.css';
 import HeroSection from '../HeroSection/HeroSection';
 import Cards from '../Card/Cards';
+import Footer from '../Footer/Footer';
 
 const Home = () => {
   return (
     <div>
       <HeroSection />
       <Cards />
+      <Footer />
     </div>
   )
 }
