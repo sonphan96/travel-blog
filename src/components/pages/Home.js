9df86911b5774b768b3016b9b18270469e2c7a6c
johnson <phantruongson96@gmail.com>
2021-03-16 15:30:24 -0400

    cards

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import '../../App.css';
 import HeroSection from '../HeroSection/HeroSection';
+import Cards from '../Card/Cards';
 
 const Home = () => {
   return (
     <div>
       <HeroSection />
+      <Cards />
     </div>
   )
 }
